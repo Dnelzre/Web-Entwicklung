67c159fe92b643a8f5c3f04905a46010a1770787
daniel <trieksx3@gmail.com>
2025-11-14 13:57:09 +0100

    Tasks border

--- app/Views/Startseite.php
+++ app/Views/Startseite.php
@@ -36,7 +36,7 @@
     </style>
     <nav class="navbar navbar-expand-lg">
         <a class="navbar-brand" href="#">
-            <img src="./assets/images/WeLogo.svg" width="150" height="50" alt="Logo">
+            <img src="./assets/images/WeLogo.svg" width="175" height="75" alt="Logo">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -55,7 +55,14 @@
             </ul>
         </div>
     </nav>
-</head>
+    <div class="container mt-4">
+        <div class="border rounded p-3 bg-white shadow-sm">
+            <h5 class="mb-1">Tasks</h5>
+            <hr>
+            <span>test</span>
+        </div>
+
+    </div>
 <body>
 <header
 
