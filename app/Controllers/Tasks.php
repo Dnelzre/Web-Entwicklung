660fe03ffffc41172f7a9c29bebdb3d1c9c17a6b
sullt <s4sudurm@uni-trier.de>
2025-12-17 12:48:06 +0100

    tasks

--- app/Controllers/Tasks.php
+++ app/Controllers/Tasks.php
@@ -5,23 +5,39 @@ namespace App\Controllers;
 use App\Models\TasksModel;
 class Tasks extends BaseController
 {
-    public function index()
+    //
+    public function __construct()
     {
-        //Model einbinden
-        $model = new TasksModel();
-
-        //Daten aus Datenbank
-        $personen = $model->getData();
-
-        $data = [
-            'personen' => $personen,
-            'title' => 'Personen Übersicht'
-        ];
-
-        // Views laden und ausgeben
-        echo view('templates/header', $data);
-        echo view('templates/menu');
-        echo view('pages/personen', $data);
-        echo view('templates/footer');
+        $this -> TasksModel = new TasksModel();
     }
+
+    public function index_edit() {
+        $data['title'] = "Bearbeiten";
+        $data['tasks'] = $this->TasksModel->gettasks();
+
+        echo view( 'templates/header');
+        echo view('pages/personen', $data);
+        echo view( 'templates/footer');
+
+    }
+
+//    public function index()
+//    {
+//        //Model einbinden
+//        $model = new TasksModel();
+//
+//        //Daten aus Datenbank
+//        $personen = $model->getData();
+//
+//        $data = [
+//            'personen' => $personen,
+//            'title' => 'Personen Übersicht'
+//        ];
+//
+//        // Views laden und ausgeben
+//        echo view('templates/header', $data);
+//        echo view('templates/menu');
+//        echo view('pages/personen', $data);
+//        echo view('templates/footer');
+//    }
 }
